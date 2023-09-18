6497f38aae72aa401995ca10aff034d964346770
tkmynrg <dlsenok@mail.ru>
2023-09-18 21:15:29 +0300

    randomNumberGenerator

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,15 @@
 import '../scss/app.scss';
 
 $(document).ready(function() {
-    function generateHexColor() {
-        let hexColor = '#';
-        let hexValues = '0123456789ABCDEF';
+    let $minValueInput = $('[data-min-value]');
+    let $maxValueInput = $('[data-max-value]');
+    let $resultDiv = $('.random__result');
+    let $generateButton = $('.generate-button');
 
-        for (let i = 0; i < 6; i++) {
-            hexColor += hexValues[Math.floor(Math.random() * 16)];
-        }
-        return hexColor;
-    }
-    let degrees = 0;
-
-    // Обработчик нажатия на кнопку
-    $('#generateButton').click(function() {
-        let color = generateHexColor();
-        let hexCode = $('#hexCode');
-
-        $('#colorContainer').css('background-color', color);
-
-        hexCode.text === '' ? hexCode.hide() : hexCode.show();
-        hexCode.text(color);
-
-        degrees += -360;
-
-        $(this).css({
-            'transition': 'transform 0.8s cubic-bezier(.55,.26,.94,.53)',
-            'transform': `rotate(${degrees}deg)`
-        });
+    $generateButton.click(function() {
+        let min = parseInt($minValueInput.val(), 10);
+        let max = parseInt($maxValueInput.val(), 10);
+        let result = Math.floor(Math.random() * (max - min + 1)) + min;
+        $resultDiv.text(result);
     });
 });
