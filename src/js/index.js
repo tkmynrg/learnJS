666dd8ccf3a75d450a0b5558b4f14911a8ad53d7
tkmynrg <dlsenok@mail.ru>
2023-09-19 01:13:50 +0300

    palindrome

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
-import '../scss/app.scss';
+// import '../scss/app.scss';
 
+function isPalindrome(str) {
+    let length = str.length;
+
+    for (let i = 0; i < (length / 2); i++) {
+        if (str[i] !== str[length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome('123322'));
