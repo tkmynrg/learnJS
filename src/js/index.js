3d6c1a188e45e13225ac7d7b8647b25db84f0d75
tkmynrg <dlsenok@mail.ru>
2023-09-19 00:57:28 +0300

    pop-up

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import '../scss/app.scss';
 
+$(document).ready(function() {
+    $('.button').click(function() {
+        $('body').addClass('popup-show');
+    });
+
+    $('.close').click(function() {
+        $('body').removeClass('popup-show');
+    });
+});
