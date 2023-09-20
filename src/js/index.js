a6c20d1313b9e5cbb3090987b9d9b104cc7406e4
tkmynrg <dlsenok@mail.ru>
2023-09-20 22:01:15 +0300

    copyText

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,15 @@
 import '../scss/app.scss';
 
 $(document).ready(function() {
+    let copyText = document.getElementById("myInput");
+    let copyBtn = document.getElementById("myButton");
 
+    copyBtn.addEventListener('click', async () => {
+        try {
+            await navigator.clipboard.writeText(copyText.value);
+            alert('Text successfully copied');
+        } catch (err) {
+            console.error('Failed to copy text: ', err);
+        }
+    });
 });
