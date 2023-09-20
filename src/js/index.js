21b1aaa33946044293d11efaaafb2efc984b26b2
tkmynrg <dlsenok@mail.ru>
2023-09-20 21:30:00 +0300

    vowels

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
 import '../scss/app.scss';
 
+$(document).ready(function() {
+    function countVowels(str) {
+
+        let count = 0;
+        const loverStr = str.toLowerCase();
+        const vowels = 'ауоиэыяюеё';
+
+        for (let i = 0; i < loverStr.length; i++) {
+            if (vowels.includes(loverStr[i])) {
+                count++
+            }
+        }
+
+        return count;
+
+    }
+    console.log(countVowels('Шла саша по шоссе и сосала сушку'))
+});
