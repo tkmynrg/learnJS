557ca726b1190cc880fdd23c96ea4ffab87e8990
tkmynrg <dlsenok@mail.ru>
2023-09-29 17:39:09 +0300

    linkGenerator finish

--- src/js/index.js
+++ src/js/index.js
@@ -24,4 +24,34 @@ $(document).ready(function() {
             $button.removeClass('disabled');
         }
     });
+
+    $('.shortener__url-input button').on('click', function(e) {
+        e.preventDefault(); // Предотвращаем стандартное действие кнопки, если оно есть.
+
+        // Проверяем, не имеет ли кнопка класс disabled. Если имеет, то выходим из функции.
+        if($(this).hasClass('disabled')) return;
+
+        // Генерируем случайную строку длиной от 6 до 20 символов
+        var randomLength = Math.floor(Math.random() * (20 - 6 + 1)) + 6;
+        var randomString = generateRandomString(randomLength);
+
+        // Формируем полную ссылку
+        var shortLink = 'https://shortened.com/' + randomString;
+
+        // Подставляем сгенерированную ссылку в .shortener__short-link-text
+        $(this).closest('.shortener').find('.shortener__short-link-text').text(shortLink);
+
+        // Находим ближайший элемент .shortener__card и добавляем ему класс visible.
+        $(this).closest('.shortener').find('.shortener__card').addClass('visible');
+    });
+
+    function generateRandomString(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
 });
